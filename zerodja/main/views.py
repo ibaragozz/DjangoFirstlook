1d7b0e6362302a87d54bd446c5d2c4c4892a352f
ibaragozz <baragozz1@gmail.com>
2024-10-24 22:46:28 +0300

    add 2page

--- zerodja/main/views.py
+++ zerodja/main/views.py
@@ -2,3 +2,6 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("<h1>Это мой первый проект на Django</h1>")
+
+def new(request):
+    return HttpResponse("<h1>Это моя вторая страница на Django</h1>")
