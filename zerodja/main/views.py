a47896c0c051e82e5ad3095985cea408cee5b2f0
ibaragozz <baragozz1@gmail.com>
2024-10-26 14:58:32 +0300

    lesson almost done

--- zerodja/main/views.py
+++ zerodja/main/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return render(request, 'main/index.html')
+    return render(request, 'main/index.html', {'caption': 'DjangoTest'})
 
 def new(request):
     return render(request, 'main/new.html')
