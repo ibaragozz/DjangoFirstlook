9f9a3fbc0f7f41af67937da2370c20c8a6bdd0b9
ibaragozz <baragozz1@gmail.com>
2024-10-26 15:03:36 +0300

    Update views.py

--- zerodja/main/views.py
+++ zerodja/main/views.py
@@ -2,7 +2,10 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return render(request, 'main/index.html', {'caption': 'DjangoTest'})
+    data = {
+        'caption': 'DjangoTest'
+    }
+    return render(request, 'main/index.html', data)
 
 def new(request):
     return render(request, 'main/new.html')
