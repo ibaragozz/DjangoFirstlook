7f55d14ff5ed60d2d66e7b23e8043750703a56a9
ibaragozz <baragozz1@gmail.com>
2024-10-27 21:41:40 +0300

    hw31test

--- zerodja/main/views.py
+++ zerodja/main/views.py
@@ -1,12 +1,28 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-def index(request):
-    data = {
-        'caption': 'DjangoTest'
-    }
-    return render(request, 'main/index.html', data)
+# def index(request):
+#     data = {
+#         'caption': 'DjangoTest'
+#     }
+#     return render(request, 'main/index.html', data)
+#
+# def new(request):
+#     return render(request, 'main/new.html')
 
-def new(request):
-    return render(request, 'main/new.html')
+
+def main_page(request):
+    return render(request, 'main_page.html')
+
+def first_breed(request):
+    return render(request, '1st.html')
+
+def second_breed(request):
+    return render(request, '2nd.html')
+
+def third_breed(request):
+    return render(request, '3rd.html')
+
+def fourth_breed(request):
+    return render(request, '4th.html')
 
