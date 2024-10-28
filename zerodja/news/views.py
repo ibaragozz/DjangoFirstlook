7cc0959d70238a622fd2468b45198d7b6344aba6
ibaragozz <baragozz1@gmail.com>
2024-10-28 21:45:18 +0300

    lesson_done

--- zerodja/news/views.py
+++ zerodja/news/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from .models import news_post
 
 # Create your views here.
 def home(request):
-    return render(request, 'news/news.html')
+    news = news_post.objects.all()
+    return render(request, 'news/news.html', {'news': news})
